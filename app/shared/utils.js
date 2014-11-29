ae0d8b49c5225db4a9877d320291d26ca6f4c72a
Brian Hines <brian@projectweekend.net>
2014-11-29 08:57:22 -0500

    Start utils to read system data

---
+++ app/shared/utils.js
@@ -0,0 +1,24 @@
+var exec = require( "child_process" ).exec;
+var async = require( "async" );
+
+
+var readCPUTemperature = function ( done ) {
+    exec( "vcgencmd measure_temp", function ( err, stdout, stderr ) {
+        if ( err ) {
+            return done( err );
+        }
+
+        var match = stdout.match(/\d+\.\d+|\d+/);
+
+        if ( !match ) {
+            return done( "No temp in stdout: " + stdout );
+        }
+
+        return done( null, parseFloat( match[ 0 ] ) );
+    } );
+};
+
+
+exports.readSystemData = function ( cb ) {
+
+};
