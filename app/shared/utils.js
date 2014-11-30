71592f089868da7318d207d51447409ce5703a10
Brian Hines <brian@projectweekend.net>
2014-11-29 20:32:35 -0600

    Let's keep it simple for now

--- app/shared/utils.js
+++ app/shared/utils.js
@@ -1,57 +1,17 @@
 var exec = require( "child_process" ).exec;
-var async = require( "async" );
 
 
-exports.readSystemData = function ( cb ) {
-    var asyncTasks = {
-        cpuTemperature: function ( done ) {
-            exec( "vcgencmd measure_temp", function ( err, stdout, stderr ) {
-                if ( err ) {
-                    return done( err );
-                }
-
-                var match = stdout.match(/\d+\.\d+|\d+/);
-                if ( !match ) {
-                    return done( new Error( "No temp in stdout: " + stdout ) );
-                }
-
-                return done( null, parseFloat( match[ 0 ] ) );
-            } );
-        },
-        gpuMemory: function ( done ) {
-            exec( "vcgencmd gpu", function ( err, stdout, stderr ) {
-                if ( err ) {
-                    return done( err );
-                }
-
-                var match = stdout.match(/\d+[A-Z]/);
-                if ( !match ) {
-                    return done( new Error( "No gpu mem in stdout: " + stdout ) );
-                }
-
-                return done( null, match );
-            } );
-        },
-        armMemory: function ( done ) {
-            exec( "vcgencmd arm", function ( err, stdout, stderr ) {
-                if ( err ) {
-                    return done( err );
-                }
-
-                var match = stdout.match(/\d+[A-Z]/);
-                if ( !match ) {
-                    return done( new Error( "No arm mem in stdout: " + stdout ) );
-                }
-
-                return done( null, match );
-            } );
-        }
-    };
-
-    async.parallel( asyncTasks, function ( err, data ) {
+exports.readCPUTemperature = function ( cb ) {
+    exec( "vcgencmd measure_temp", function ( err, stdout, stderr ) {
         if ( err ) {
-            return cb( err );
+            return done( err );
         }
-        return cb( null, data );
+
+        var match = stdout.match(/\d+\.\d+|\d+/);
+        if ( !match ) {
+            return done( new Error( "No temp in stdout: " + stdout ) );
+        }
+
+        return done( null, parseFloat( match[ 0 ] ) );
     } );
 };
